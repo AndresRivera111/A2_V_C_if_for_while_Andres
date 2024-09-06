1a51d6ad9d33260f87c0e888e47ddcab8182a117
AndresRivera111 <a.22308051281295@cbtis128.edu.mx>
2024-09-06 09:32:54 -0600

    Add files via upload
    subí dos archivos

---
+++ micadenas.py
@@ -0,0 +1,23 @@
+'''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+
+nombre = "Andres"
+print(nombre[0])
+print(nombre)
+MisNovias=["asdbfñu","iaugd","jagdsi"]
+for chicas in MisNovias:
+  print(chicas)
+
+  print("otro ejemplo")
+  b = "Andres Rivera"
+print(b[2:3])
+print("a mayusculas")
+a = "Andres Rivera"
+print(a.upper())
+print("concatenar")
+a = "Andres"
+b = "Rivera"
+c = a + " " + b
+print(c)
